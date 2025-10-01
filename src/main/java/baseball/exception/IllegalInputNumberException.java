ddde11624b6740beab59622c3ecc11e2c549b96e
IkJuLim <limik1004@gmail.com>
2025-10-01 09:16:52 +0900

    feat: (IllegalInputNumberException) 생성

---
+++ src/main/java/baseball/exception/IllegalInputNumberException.java
@@ -0,0 +1,7 @@
+package baseball.exception;
+
+public class IllegalInputNumberException extends IllegalArgumentException {
+    public IllegalInputNumberException(String message) {
+        super(message);
+    }
+}
